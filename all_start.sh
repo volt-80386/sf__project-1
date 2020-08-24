eb011d0a87bb933a75a8e8275898e3222836e39b
volt-80386 <39161387+volt-80386@users.noreply.github.com>
2020-08-24 17:13:38 +0300

    Add files via upload

---
+++ all_start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export LOG=/srv/project1.log 
+cd /srv
+echo "--------------- START ---------------" >> $LOG
+echo $(date +"%F %H:%M:%S") >> $LOG
+echo "---- sync start ----" >> $LOG
+make sync >> $LOG
+echo "---- build start ----" >> $LOG
+make build >> $LOG
+echo "---- deploy start ----" >> $LOG
+make deploy >> $LOG
+echo "--------------- END ---------------" >> $LOG
